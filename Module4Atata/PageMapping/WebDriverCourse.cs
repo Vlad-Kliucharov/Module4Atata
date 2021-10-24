17b20bb68fcf6ac4bdcc4f868ea57e05148cceef
kliucharovvlad@gmail.com <kliucharovvlad@gmail.com>
2021-10-24 22:48:09 +0300

    Added PageMapping objects

---
+++ Module4Atata/PageMapping/WebDriverCourse.cs
@@ -0,0 +1,12 @@
+﻿using Atata;
+
+namespace Module4Atata.PageMapping
+{
+    using _ = WebDriverCourse;
+
+    public class WebDriverCourse : Page<_>
+    {
+        [FindByContent("Поиск элементов на странице")]
+        public Control<_> ArticlesSearchElements { get; private set; }
+    }
+}
---
+++ Module4Atata/PageMapping/YeskMainPage.cs
@@ -0,0 +1,14 @@
+﻿using Atata;
+using NUnit.Framework;
+
+namespace Module4Atata.PageMapping
+{
+    public class YeskMainPage : ConfigTest
+    {
+        [Test]
+        public void MainPage()
+        {
+            Go.ToUrl("http://www.yesk.com.ua/");
+        }
+    }
+}
